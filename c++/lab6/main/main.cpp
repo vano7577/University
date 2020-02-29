b4b4025d9ec4da52a5c144dd115626d5a149d65c
Vano7577 <43076526+vano7577@users.noreply.github.com>
2020-02-29 15:44:29 +0200

    Add files via upload

---
+++ c++/lab6/main/main.cpp
@@ -0,0 +1,259 @@
+﻿// main.cpp : Определяет точку входа для приложения.
+//
+
+#include "stdafx.h"
+#include "main.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+INT_PTR CALLBACK Main(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+int SendCopyData(HWND, HWND, void *lp, long);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Разместите код здесь.
+
+    // Инициализация глобальных строк
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MAIN, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Выполнить инициализацию приложения:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MAIN));
+
+    MSG msg;
+
+    // Цикл основного сообщения:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MAIN));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MAIN);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      0, 0, 300, 300, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Разобрать выбор в меню:
+            switch (wmId)
+            {
+			case ID_RUN:
+				DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG), hWnd, Main);
+				break;
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+	{
+		HWND hWndDataCreator = FindWindow("OBJ2", NULL);
+		HWND hWndDataCreator2 = FindWindow("OBJ3", NULL);
+		PostMessage(hWndDataCreator, WM_DESTROY, 10000, (long)hWnd);
+		PostMessage(hWndDataCreator2, WM_DESTROY, 10000, (long)hWnd);
+		PostQuitMessage(0);
+	}
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+INT_PTR CALLBACK Main(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		//long n, minx, miny, maxx, maxy;
+		long params[5];
+		BOOL isRead = FALSE;
+		HWND hWndDataCreator;
+		HWND hWndDataCreator2;
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			params[0] = GetDlgItemInt(hDlg, IDC_N, (BOOL *)isRead, FALSE);
+			//isRead = TRUE;
+			params[1] = GetDlgItemInt(hDlg, IDC_MINX, (BOOL *)isRead, TRUE);
+			params[3] = GetDlgItemInt(hDlg, IDC_MINY, (BOOL *)isRead, TRUE);
+			params[2] = GetDlgItemInt(hDlg, IDC_MAXX, (BOOL *)isRead, TRUE);
+			params[4] = GetDlgItemInt(hDlg, IDC_MAXY, (BOOL *)isRead, TRUE);
+
+			hWndDataCreator = FindWindow("OBJ2", NULL);
+			hWndDataCreator2 = FindWindow("OBJ3", NULL);
+
+			if (hWndDataCreator && hWndDataCreator2)
+			{
+				SendCopyData(hWndDataCreator, hDlg, params, sizeof(params));
+				SendCopyData(hWndDataCreator2, hDlg, params, sizeof(params));
+			}
+			else {
+				WinExec("\"F:\\Vano\\proga\\ООП\\lab62\\main\\Debug\\Obj2.exe\"", SW_SHOW);
+				hWndDataCreator = FindWindow("OBJ2", NULL);
+				SendCopyData(hWndDataCreator, hDlg, params, sizeof(params));
+				WinExec("\"F:\\Vano\\proga\\ООП\\lab62\\main\\Debug\\Obj3.exe\"", SW_SHOW);		
+				hWndDataCreator2 = FindWindow("OBJ3", NULL);	
+				SendCopyData(hWndDataCreator2, hDlg, params, sizeof(params));
+			}
+
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+			break;
+		case IDCANCEL:
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+			break;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
+
+
+
+int SendCopyData(HWND hWndDest, HWND hWndSrc, void *lp, long cb) {
+	COPYDATASTRUCT cds;
+
+	cds.dwData = 1;    //а можна і будь-яке інше значення 
+	cds.cbData = cb;
+	cds.lpData = lp;
+	return SendMessage(hWndDest, WM_COPYDATA, (WPARAM)hWndSrc, (LPARAM)&cds);
+}
